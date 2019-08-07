65f5d02091462a2e589b3adffe828e2ebdc2574d
Nick Gal <nickgal@users.noreply.github.com>
2019-08-07 14:11:30 -0700

    Update rails.rb

--- lib/switch_user/rails.rb
+++ lib/switch_user/rails.rb
@@ -4,6 +4,7 @@ module SwitchUser
   class Engine < Rails::Engine
     initializer 'switch_user.view' do
       ActiveSupport.on_load(:action_view) do
+        require 'switch_user_helper'
         include SwitchUserHelper
       end
     end
